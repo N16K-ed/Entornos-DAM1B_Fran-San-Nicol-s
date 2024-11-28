f2bafaf2c1531d362acfd45a18a11b2899cc2384
Fran <fsannicceb@educacion.navarra.com>
2024-11-28 21:05:55 +0100

    comentarios de Java doc de todas las clases

--- juegoPokemon/src/AppPokemon.java
+++ juegoPokemon/src/AppPokemon.java
@@ -1,5 +1,12 @@
+/**
+ * Clase AppPokemon (main).
+ * Se encarga de ejecutar el juego.
+ */
 public class AppPokemon {
-
+/**
+ * Método main.
+ * Crea una nueva interfáz y lláma al método Juego de la clase InterfazPokemon {@link InterfazPokemon}.
+ */
     public static void main(String[] args) {
         InterfazPokemon juegoPokemon = new InterfazPokemon();
         juegoPokemon.Juego();
